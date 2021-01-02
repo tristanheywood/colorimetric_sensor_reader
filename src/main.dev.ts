86971b85193cc5f93d68939f4284769b8cf00c87
Tristan Heywood <tristanheywood@hotmail.com>
2021-01-02 21:23:41 +1100

    detect python server running; allow python logging through nodejs

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -16,6 +16,15 @@ import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 
+const WebSocket = require('ws')
+
+function print(...args: any[]) {
+  log.warn(...args);
+  // console.log(...args);
+}
+
+console.log()
+
 export default class AppUpdater {
   constructor() {
     log.transports.file.level = 'info';
@@ -134,33 +143,68 @@ app.on('activate', () => {
 
 app.on('ready', () => {
 
-  let backendDistPath = process.env.NODE_ENV === 'production' ?
-  path.join(process.resourcesPath, 'sotcat_backend_dist') : path.join(__dirname, '..', 'sotcat_backend_dist');
+  function start_python_server() {
+    let backendDistPath = process.env.NODE_ENV === 'production' ?
+    path.join(process.resourcesPath, 'sotcat_backend_dist') : path.join(__dirname, '..', 'sotcat_backend_dist');
 
-  // let fullPath = path.join(__dirname, '..',  'sotcat_backend_dist');
-  log.warn("Looking for packaged backend at: ", backendDistPath);
-  let backendIsPackaged = require('fs').existsSync(backendDistPath);
+    // let fullPath = path.join(__dirname, '..',  'sotcat_backend_dist');
+    print("Looking for packaged backend at: ", backendDistPath);
+    let backendIsPackaged = require('fs').existsSync(backendDistPath);
 
-  if (!backendIsPackaged) {
-    log.warn("No packaged backend detected, attempting to run python backend server");
+    if (!backendIsPackaged) {
+      print("No packaged backend detected, attempting to run python backend server");
 
-    var subpy = require('child_process').spawn('python', ['./src/sotcat_backend/server.py'])
+      var subpy = require('child_process').spawn('python', ['./src/sotcat_backend/server.py'])
 
-    subpy.stdout.on('data', (data: any) => {
-      log.warn(data.toString());
-    });
+      subpy.stdout.on('data', (data: any) => {
+        print(data.toString());
+      });
 
-  } else {
-    log.warn("Running packaged backend server");
+    } else {
+      print("Running packaged backend server");
 
-    let scriptPath = path.join(backendDistPath, 'server', 'server.exe');
-    log.warn('Executing script: ', scriptPath);
-    var subpy = require('child_process').exec(scriptPath);
+      let scriptPath = path.join(backendDistPath, 'server', 'server.exe');
+      print('Executing script: ', scriptPath);
+      var subpy = require('child_process').exec(scriptPath);
 
-    subpy.stdout.setEncoding('utf8')
-    subpy.stdout.on('data', function(data: any) {
-      log.warn(data.toString());
-    });
+      subpy.stdout.setEncoding('utf8')
+      subpy.stdout.on('data', function(data: any) {
+        print(data.toString());
+      });
+    }
   }
+
+  let pythonWS = new WebSocket('ws://localhost:8001');
+
+  function connect_to_python_ws() {
+
+    // delay to let python server start
+    setTimeout(() => {
+      pythonWS = new WebSocket('ws://localhost:8001');
+
+      // delay to let websocket connect
+      setTimeout(() => {
+        if (pythonWS.readyState != WebSocket.CONNECTED) {
+          print('Error: failed to connect to Python websocket');
+        }
+      })
+    }, 0.1);
+  }
+
+
+  setTimeout(() => {
+    if (pythonWS.readyState == WebSocket.CLOSED) {
+      print('no Python server detected after 0.1 seconds, starting one...');
+
+      start_python_server();
+      connect_to_python_ws();
+    } else {
+      print('Existing Python server detected.');
+    }
+  }, 0.1);
+
+  pythonWS.on('message', (msg: string) => {
+    print('[Python] ', msg);
+  });
 
 })
